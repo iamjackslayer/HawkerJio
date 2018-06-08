941c82c5b5d99d0d9e924d7845c0b7319dd03044
Jack Wong <jackwong3101@yahoo.com>
2018-06-08 22:08:27 +0800

    edited user_home_page: changed layout

--- lib/user_home_page.dart
+++ lib/user_home_page.dart
@@ -15,21 +15,20 @@ class UserHomePage extends StatefulWidget {
 
 class _UserHomePageState extends State<UserHomePage> with SingleTickerProviderStateMixin {
 
-
+  Color _orderButtonColor;
+  Color _deliverButtonColor;
 
   @override
   void initState() {
     super.initState();
+    _orderButtonColor = Colors.pink;
+    _deliverButtonColor = Colors.white;
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: null,
-        backgroundColor: Color.fromRGBO(247, 64, 106, 1.0),
-      ),
-
+      backgroundColor: Colors.white70,
       body: new Center(
               child: new Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -40,41 +39,150 @@ class _UserHomePageState extends State<UserHomePage> with SingleTickerProviderSt
                           child: new Text(
                             'Hawker Jio',
                             style: new TextStyle(
-                              fontSize: 20.0
+                              fontSize: 50.0
                             ),
                           ),
                         ),
-
-                        new Padding(
-                          padding: new EdgeInsets.all(5.0),
-                          child: new Text(
-                            'I wanna get food...',
-                            style: new TextStyle(
-                              fontSize: 15.0,
-
-                            ),
+                        new Container(
+                          margin: new EdgeInsets.fromLTRB(45.0, 0.0, 45.0, 10.0),
+                          decoration: new BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: new BorderRadius.all(const Radius.circular(20.0)),
                           ),
+
+                          child: new Row(
+                            children: <Widget>[
+                              new Column(
+                                children: <Widget>[
+                                  new Padding(
+                                    padding: new EdgeInsets.fromLTRB(15.0, 5.0, 10.0, 0.0),
+                                    child:new Icon(Icons.free_breakfast),
+                                  ),
+                                  new Padding(
+                                    padding: new EdgeInsets.fromLTRB(15.0, 0.0, 10.0, 0.0),
+                                    child:new Icon(Icons.more_vert),
+                                  ),
+                                  new Padding(
+                                    padding: new EdgeInsets.fromLTRB(15.0, 0.0, 10.0, 5.0),
+                                    child:new Icon(Icons.free_breakfast),
+                                  ),
+                                ],
+                              ),
+                              new Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  new Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      new Text("From: ",
+                                        style: new TextStyle(
+                                          fontSize: 17.0,
+                                          fontWeight: FontWeight.bold
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  new Container(
+                                    margin: new EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                                    width: 220.0,
+                                    height: 2.0,
+                                    color: Colors.black12,
+                                  ),
+                                  new Row(
+                                    children: <Widget>[
+                                      new Text("To:",
+                                          style: new TextStyle(
+                                              fontSize: 17.0,
+                                              fontWeight: FontWeight.bold
+                                          )
+                                      )
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          )
                         ),
 
-                      // Below is two Jumbo sized buttons
 
-                        new Padding(
-                          padding: new EdgeInsets.all(10.0),
-                          child: new InkWell(
-                            child: new MyButton('Passively', height: 180.0,),
-                            onTap: () {
-                              Navigator.pushNamed(context, '/JioListPage');
-                            },
-                          ),
-                        ),
-                        new Padding(
-                            padding: new EdgeInsets.all(10.0),
-                            child: new InkWell(
-                              child: new MyButton('Actively', height: 180.0,),
-                              onTap: () {
-                                Navigator.pushNamed(context, '/CreateJioPage');
+                      // This row contains two radio buttons "order" and "deliver"
+
+                       new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+
+                          new Container(
+
+                            child: new FlatButton(
+                              child: new Container(
+                                child:  new Text("Order"),
+                              ),
+                              onPressed: (){
+                                setState(() {
+                                  _orderButtonColor = Colors.pink;
+                                  _deliverButtonColor = Colors.white;
+                                });
                               },
                             ),
+                            decoration: new BoxDecoration(
+                              borderRadius: new BorderRadius.only(topLeft: new Radius.circular(18.0), bottomLeft: new Radius.circular(18.0)),
+                              color: _orderButtonColor,
+                            ),
+                          ),
+                          //A vertical divider in between the two buttons.
+                          new Container(
+                            width: 2.0,
+                            height: 25.0,
+                            color: Colors.black12 ,
+                          ),
+
+                          new Container(
+                            child: new FlatButton(
+                              child: new Container(
+                                child:  new Text("Deliveries"),
+                                color: Colors.transparent,
+                              ),
+                              onPressed: (){
+                                setState(() {
+                                  _deliverButtonColor = Colors.pink;
+                                  _orderButtonColor = Colors.white;
+
+                                });
+                              },
+
+                            ),
+                            decoration: new BoxDecoration(
+                              borderRadius: new BorderRadius.only(topRight: new Radius.circular(18.0), bottomRight: new Radius.circular(18.0)),
+                              color: _deliverButtonColor,
+                            ),
+                          ),
+
+                        ],
+                      ),
+
+
+
+
+                       // Button that navigates to another route
+                        new Container(
+                          margin: new EdgeInsets.only(top: 200.0),
+                          child: new FlatButton(
+                            onPressed: (){
+                              if (_orderButtonColor == Colors.pink) {
+                                Navigator.pushNamed(context, "/JioListPage");
+                              } else if (_deliverButtonColor == Colors.pink) {
+
+                              }
+                            },
+                            child: new Icon(Icons.arrow_forward_ios),
+
+                          ),
+                          decoration: new BoxDecoration(
+                            borderRadius: new BorderRadius.all(const Radius.circular(18.0)),
+                            color: Colors.white,
+                          ),
+
+
                         )
 
                       ],
