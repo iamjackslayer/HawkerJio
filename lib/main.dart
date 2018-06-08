93bb7d335062d4cbaa07251c7cf0e2e98bd288a7
Nyxf4ll_ <vosk1093@gmail.com>
2018-06-08 16:00:26 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
         // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: new WelcomePage(),
       routes: <String, WidgetBuilder> {
